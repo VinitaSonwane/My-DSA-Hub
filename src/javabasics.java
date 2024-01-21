9e0b6a199714c9fd7e17ce40b551884d4f772b18
VinitaSonwane <VinitaSonwane>
2024-01-21 15:12:28 +0530

    first commit

---
+++ src/javabasics.java
@@ -0,0 +1,73 @@
+public class javabasics{
+    public static void main(String[]args){
+        /*/*int a=10;
+        int b=23;
+        int sum = a+b;
+      System.out.println(sum);
+        System.out.println(a);
+        System.out.print("****\n");
+        System.out.print("***\n");
+        System.out.print("**\n");
+        System.out.print("*");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        String names = sc.nextLine();
+        System.out.println(names);*/
+
+       /* Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b=sc.nextInt();
+        int product =a*b;
+        System.out.println("the product of num : " + product);*/
+
+        /* Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b=sc.nextInt();
+        int sum =a+b;
+        System.out.println("the product of num : " + sum);*/
+
+
+        //AREA OF CIRCLE
+        /*float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad ;
+        System.out.println(area);*/
+
+        //TYPE CASTING ( CONVERT ONE DATA TYPE INTO OTHER)
+        /*float number = 99.9f;
+        int number2 = (int) number;
+        System.out.println(number2);*/
+
+        
+        /*int num = 67;
+        char ch = (char) num;
+         System.out.println(ch); 
+        char a ='a';
+        char b = 'b';
+        System.out.println((int)(b));
+           System.out.println((int)(a));
+              System.out.println(b-a);
+
+              float num2 = 23.4f;
+              double num3 = (double)num2;
+              System.out.println(num3 );
+
+              byte p =5;
+              byte q =(byte)(p*2);
+              System.out.println(q);*/
+
+
+
+        //ARITHMETIC OPERATOR
+        int a =10;
+        //int b =++A;
+        int  b = a--;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println((3>2) && (30>24));     //Logical and
+        System.out.println((3>4) || (4<5));       //Logical OR
+
+
+
+    }
+}
