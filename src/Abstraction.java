ee3264753c16e6d2320001f707a625f79027f668
VinitaSonwane <VinitaSonwane>
2024-01-21 14:27:59 +0530

    first commit

---
+++ src/Abstraction.java
@@ -0,0 +1,36 @@
+abstract class Animal {
+    abstract void walk();
+    Animal(){             //constructor
+        System.out.println("You are created a animal");
+    }
+    public void eat(){
+        System.out.println("Animal eats");
+    }
+
+}
+
+class Horse extends Animal {
+    Horse(){
+        System.out.println("Horse is created");
+    }
+    public void walk() {
+        System.out.println("walk on 4 legs");
+    }
+}
+
+class Chicken extends Animal {
+    public void walk() {
+        System.out.println("walk on 2 legs");
+    }
+}
+
+public class Abstraction {
+    public static void main(String[] args) {
+        Horse horse = new Horse();
+        // horse.walk();
+        //horse.eat();
+
+
+    }
+}
+
