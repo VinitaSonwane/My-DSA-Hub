b64ae61c809f0c60cd24217eeeddd3feb65f548b
VinitaSonwane <VinitaSonwane>
2024-01-21 15:17:36 +0530

    first commit

---
+++ src/Pattern.java
@@ -0,0 +1,38 @@
+public class Pattern {
+    public static void main(String[]args){
+        //Nested loops
+
+        /*//print pattern of star
+        for(int line = 1 ; line <= 4 ; line ++){
+            for(int star = 1 ; star<= line ; star++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }*/
+
+       /* //PRINT HALF PYRAMID
+        int n = 4;
+        for(int line =1 ; line <=n ; line++){
+            for(int number=1 ; number<=line ; number++){
+                System.out.print(" "+number);
+            }
+            System.out.println();
+        }*/
+
+        /*//PRINT CHARACTER PATTERN
+        char ch = 'A';
+        int n= 4;
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }*/
+
+
+
+    }
+
+}
